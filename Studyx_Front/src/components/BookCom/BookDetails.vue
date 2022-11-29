e52ace8a523fd4f8d146025eb279d7250c8c742a
babyseal <75252510+babyseal1121@users.noreply.github.com>
2022-11-29 20:31:18 +0800

    Update BookDetails.vue

--- Studyx_Front/src/components/BookCom/BookDetails.vue
+++ Studyx_Front/src/components/BookCom/BookDetails.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
   <el-card :body-style="{ padding: '0px' }">
     <img :src="books[0].cover" alt="封面">
     <div style="padding: 14px;">
@@ -13,6 +14,7 @@
       </div>
     </div>
   </el-card>
+  </div>
 </template>
 
 <script>
