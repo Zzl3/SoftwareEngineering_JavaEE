613be55d720231ded5378aa24212c75c62aa91b0
babyseal <75252510+babyseal1121@users.noreply.github.com>
2022-11-29 11:35:49 +0800

    Update BookDetails.vue

--- Studyx_Front/src/components/BookCom/BookDetails.vue
+++ Studyx_Front/src/components/BookCom/BookDetails.vue
@@ -4,9 +4,9 @@
     <div style="padding: 14px;">
       <h2 class="title">{{ books[0].title }}</h2>
       <p>{{books[0].author}}</p>
-      <p>{{books[0].date}}</p>
-      <p>{{books[0].press}}</p>
-      <p>{{books[0].abs}}</p>
+      <p>{{books[0].publishdate}}</p>
+      <p>{{books[0].publisher}}</p>
+      <p>{{books[0].bookabstract}}</p>
 
       <div class="bottom clearfix">
         <el-button @click="goBack" type="text" class="button">返回</el-button>
@@ -20,36 +20,48 @@ export default {
   name: "BookDetails",
   data() {
     return {
-      books: [{
-        cover: 'https://i.loli.net/2019/04/10/5cada7e73d601.jpg',
-        title: '三体',
-        author: '刘慈欣',
-        date: '2019-05-05',
-        press: '重庆出版社',
-        abs: '文化大革命如火如荼进行的同时。军方探寻外星文明的绝秘计划“红岸工程”取得了突破性进展。但在按下发射键的那一刻，历经劫难的叶文洁没有意识到，她彻底改变了人类的命运。地球文明向宇宙发出的第一声啼鸣，以太阳为中心，以光速向宇宙深处飞驰……'
-      }],
+      books: [],
       currentPage: 1,
       pagesize: 18
     };
   },
-  methods: {
-    goBack(){
-      this.$router.replace('/book')
-    },
+  mounted: function () {
+    this.loadBooks()
   },
-  book () {
-    this.$axios
-      .post('/book', {
-        username: this.loginForm.username,
-        password: this.loginForm.password
-      })
-      .then(successResponse => {
-        if (successResponse.data.code === 200) {
-          this.$router.replace({path: '/index'})
-        }
-      })
-      .catch(failResponse => {
-      })
+  methods: {
+    loadBooks() {
+      var _this = this
+
+      this.$axios
+          .get('/search?keywords='+_this.$route.query.id,{
+          })
+          .then(resp => {
+            if (resp && resp.data.code === 200) {
+              _this.books = resp.data.result
+              this.$message.error("共" + _this.books.length + "条");
+            }
+          })
+          .catch(failResponse => {
+            this.$message.error("数据发送失败");
+          })
+    },
+    goBack(){
+      this.$router.replace('/library')
+    },
+    book () {
+      this.$axios
+          .post('/book', {
+            username: this.loginForm.username,
+            password: this.loginForm.password
+          })
+          .then(successResponse => {
+            if (successResponse.data.code === 200) {
+              this.$router.replace({path: '/index'})
+            }
+          })
+          .catch(failResponse => {
+          })
+    },
   },
 }
 </script>
@@ -86,3 +98,4 @@ export default {
 }
 
 </style>
+
