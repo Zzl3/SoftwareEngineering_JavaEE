5dd7e867e6f8e68097f74502ed9e4923a486bcae
Zzl3 <91377542+Zzl3@users.noreply.github.com>
2022-12-01 19:13:38 +0800

    .

--- Studyx_Front/src/components/AllbookCom/Echart1.vue
+++ Studyx_Front/src/components/AllbookCom/Echart1.vue
@@ -1,9 +1,12 @@
 <template>
-  <div style="width: 1000px; height: 600px;left:300px;margin-top:50px" id="main"></div>
+  <div
+    style="width: 500px; height: 300px; left: 0px; margin-top: 50px"
+    id="main"
+  ></div>
 </template>
-  
+
 <script>
-import global from '../../utils/global.js'
+import global from "../../utils/global.js";
 
 //通过this.$echarts来使用
 export default {
@@ -14,9 +17,29 @@ export default {
   },
   data() {
     return {
-      chart_data:[]
+      chart_data: [],
+      myChart: null,
     };
   },
+  //数据自动刷新，必然需要一个监听机制告诉Echarts重新设置数据
+  // watch: {
+  //   //观察option的变化
+  //   option: {
+  //     handler(newVal, oldVal) {
+  //       if (this.myChart) {
+  //         if (newVal) {
+  //           this.myChart.setOption(newVal);
+  //         } else {
+  //           this.myChart.setOption(oldVal);
+  //         }
+  //       } else {
+  //         this.echartsInit();
+  //       }
+  //     },
+  //     deep: true, //对象内部属性的监听，关键。
+  //   },
+  // },
+
   methods: {
     //初始化echarts
     echartsInit() {
@@ -25,24 +48,25 @@ export default {
       //这里的所有echarts都需要改成this.$echarts
       let myChart = this.$echarts.init(document.getElementById("main"));
       //let min=300,max=660;
-      
+
       this.$axios({
-        url:"/user/countborrow",
-        method:"get",
+        url: "/user/countborrow",
+        method: "get",
         params: {
-          id:global.nowuserid
+          id: global.nowuserid,
+        },
+      }).then((res) => {
+        for (let val of res.data) {
+          //console.log(res.data)
+          _this.chart_data.push(val * 100 + 300);
+          //console.log(_this.chart_data);
         }
-      }).then((res)=>{
-        for(let val of res.data){
-          this.chart_data.push(val*100+300);
-        }
-        // console.log(this.chart_data);
-      })
+      });
 
       var option = {
         title: {
           text: "借阅统计",
-          subtext: "可以统计一下过去一年中的借阅数量",
+          //subtext: "可以统计一下过去一年中的借阅数量",
         },
         tooltip: {
           trigger: "axis",
@@ -106,8 +130,8 @@ export default {
             type: "line",
             smooth: true,
             // prettier-ignore
-            data: this.chart_data,
-            // data: [300, 280, 250, 260, 270, 300, 550, 500, 400, 390, 380, 390, 400, 500, 600, 750, 800, 700, 600, 400],
+            //data: _this.chart_data,
+            data: [300, 280, 250, 260, 270, 300, 550, 500, 400, 390, 380, 390],
             markArea: {
               itemStyle: {
                 color: "rgba(255, 173, 177, 0.4)",
@@ -115,7 +139,7 @@ export default {
               data: [
                 [
                   {
-                    name: "Morning Peak",
+                   // name: "Morning Peak",
                     xAxis: "2月",
                   },
                   {
@@ -124,7 +148,7 @@ export default {
                 ],
                 [
                   {
-                    name: "Evening Peak",
+                   // name: "Evening Peak",
                     xAxis: "7月",
                   },
                   {
@@ -138,10 +162,10 @@ export default {
       };
       // 使用刚指定的配置项和数据显示图表。
       console.log("not here?");
-      console.log(this.chart_data);
+      //console.log(_this.chart_data);
+      console.log(option);
       myChart.setOption(option);
     },
   },
 };
 </script>
-  
--- Studyx_Front/src/components/AllbookCom/Echart2.vue
+++ Studyx_Front/src/components/AllbookCom/Echart2.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    style="width: 1000px; height: 600px; margin-left: 300px; margin-top: 100px"
+    style="width: 500px; height: 300px; margin-left: 1000px; margin-top: -300px"
     id="main2"
   ></div>
 </template>
@@ -34,45 +34,10 @@ export default {
       var option = {
         color: ["#67F9D8", "#FFE434", "#56A3F1", "#FF917C"],
         title: {
-          text: "类别统计，可以统计一下我们的图书类别",
+          text: "类别统计",
         },
         legend: {},
         radar: [
-          {
-          //   // indicator: [
-          //   //   { text: "Indicator1" },
-          //   //   { text: "Indicator2" },
-          //   //   { text: "Indicator3" },
-          //   //   { text: "Indicator4" },
-          //   //   { text: "Indicator5" },
-          //   // ],
-          //   // center: ["25%", "50%"],
-          //   // radius: 120,
-          //   // startAngle: 90,
-          //   // splitNumber: 4,
-          //   // shape: "circle",
-          //   // axisName: {
-          //   //   formatter: "【{value}】",
-          //   //   color: "#428BD4",
-          //   // },
-          //   // splitArea: {
-          //   //   areaStyle: {
-          //   //     color: ["#77EADF", "#26C3BE", "#64AFE9", "#428BD4"],
-          //   //     shadowColor: "rgba(0, 0, 0, 0.2)",
-          //   //     shadowBlur: 10,
-          //   //   },
-          //   // },
-          //   // axisLine: {
-          //   //   lineStyle: {
-          //   //     color: "rgba(211, 253, 250, 0.8)",
-          //   //   },
-          //   // },
-          //   // splitLine: {
-          //   //   lineStyle: {
-          //   //     color: "rgba(211, 253, 250, 0.8)",
-          //   //   },
-          //   // },
-          },
           {
             indicator: [
               { text: "诗歌", max: 600 },
@@ -82,75 +47,105 @@ export default {
               { text: "小说", max: 600 },
               { text: "传记", max: 600 },
             ],
-            center: ["75%", "50%"],
+            center: ["25%", "50%"],
             radius: 120,
+            startAngle: 90,
+            splitNumber: 4,
+            shape: "circle",
             axisName: {
-              color: "#fff",
-              backgroundColor: "#666",
-              borderRadius: 3,
-              padding: [3, 5],
+              formatter: "【{value}】",
+              color: "#428BD4",
+            },
+            splitArea: {
+              areaStyle: {
+                color: ["#77EADF", "#26C3BE", "#64AFE9", "#428BD4"],
+                shadowColor: "rgba(0, 0, 0, 0.2)",
+                shadowBlur: 10,
+              },
+            },
+            axisLine: {
+              lineStyle: {
+                color: "rgba(211, 253, 250, 0.8)",
+              },
+            },
+            splitLine: {
+              lineStyle: {
+                color: "rgba(211, 253, 250, 0.8)",
+              },
             },
           },
+          // {
+          //   indicator: [
+          //     { text: "诗歌", max: 600 },
+          //     { text: "心理学", max: 600 },
+          //     { text: "童话", max: 600 },
+          //     { text: "社会学", max: 600 },
+          //     { text: "小说", max: 600 },
+          //     { text: "传记", max: 600 },
+          //   ],
+          //   center: ["75%", "50%"],
+          //   radius: 120,
+          //   axisName: {
+          //     color: "#fff",
+          //     backgroundColor: "#666",
+          //     borderRadius: 3,
+          //     padding: [3, 5],
+          //   },
+          // },
         ],
         series: [
+          {
+            type: "radar",
+            emphasis: {
+              lineStyle: {
+                width: 4,
+              },
+            },
+            data: [
+              {
+                value: [63, 552, 33,63, 187,0],
+               // name: "图书数量",
+              },
+             
+            ],
+          },
           // {
           //   type: "radar",
-          //   emphasis: {
-          //     lineStyle: {
-          //       width: 4,
-          //     },
-          //   },
+          //   radarIndex: 1,
           //   data: [
           //     {
-          //       value: [100, 8, 0.4, -80, 2000],
-          //       name: "Data A",
+          //       value: num,
+          //       name: "图书类型",
+          //       symbol: "rect",
+          //       symbolSize: 12,
+          //       lineStyle: {
+          //         type: "dashed",
+          //       },
+          //       label: {
+          //         show: true,
+          //         formatter: function (params) {
+          //           return params.value;
+          //         },
+          //       },
           //     },
           //     {
-          //       value: [60, 5, 0.3, -100, 1500],
-          //       name: "Data B",
+          //       value: [1020, 933, 530, 930, 730, 603],
+          //       name: "Data D",
           //       areaStyle: {
-          //         color: "rgba(255, 228, 52, 0.6)",
+          //         color: new _this.$echarts.graphic.RadialGradient(0.1, 0.6, 1, [
+          //           {
+          //             color: "rgba(255, 145, 124, 0.1)",
+          //             offset: 0,
+          //           },
+          //           {
+          //             color: "rgba(255, 145, 124, 0.9)",
+          //             offset: 1,
+          //           },
+          //         ]),
           //       },
           //     },
           //   ],
           // },
-          {
-            type: "radar",
-            radarIndex: 1,
-            data: [
-              {
-                value: num,
-                name: "图书类型",
-                symbol: "rect",
-                symbolSize: 12,
-                lineStyle: {
-                  type: "dashed",
-                },
-                label: {
-                  show: true,
-                  formatter: function (params) {
-                    return params.value;
-                  },
-                },
-              },
-              // {
-              //   value: [100, 93, 50, 90, 70, 60],
-              //   name: "Data D",
-              //   areaStyle: {
-              //     color: new _this.$echarts.graphic.RadialGradient(0.1, 0.6, 1, [
-              //       {
-              //         color: "rgba(255, 145, 124, 0.1)",
-              //         offset: 0,
-              //       },
-              //       {
-              //         color: "rgba(255, 145, 124, 0.9)",
-              //         offset: 1,
-              //       },
-              //     ]),
-              //   },
-              // },
-            ],
-          },
         ],
       };
       // 使用刚指定的配置项和数据显示图表。
